eff645f03d24ecfc4209a61a47e1c2b202fac8f0
Matheus Romão <romaomb@outlook.com>
2023-03-18 01:53:47 -0300

    Add BlocProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pokedex_app/presentation/app.dart';
+import 'package:pokedex_app/presentation/features/pokedex/pokedex_bloc.dart';
 
-void main() => runApp(const PokedexApp());
+void main() {
+  runApp(
+    MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => PokedexBloc()),
+      ],
+      child: const PokedexApp(),
+    ),
+  );
+}
