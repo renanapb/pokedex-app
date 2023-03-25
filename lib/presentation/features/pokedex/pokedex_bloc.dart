27423cdae8c160ca731d8fe506df655b9bb44da0
Matheus Romão <romaomb@outlook.com>
2023-03-25 20:57:45 -0300

    Add basic code to demonstrate whole flow

--- lib/presentation/features/pokedex/pokedex_bloc.dart
+++ lib/presentation/features/pokedex/pokedex_bloc.dart
@@ -1,9 +1,25 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pokedex_app/domain/usecases/get_basic_pokemons_use_case.dart';
 import 'package:pokedex_app/presentation/features/pokedex/pokedex_events.dart';
-import 'package:pokedex_app/presentation/features/pokedex/pokedex_states.dart';
+import 'package:pokedex_app/presentation/features/pokedex/pokedex_state.dart';
 
 class PokedexBloc extends Bloc<PokedexEvent, PokedexState> {
-  PokedexBloc() : super(const PokedexState()) {
-    on<PokedexEvent>((event, emit) {});
+  final GetBasicPokemonsUseCase _basicPokemonsUseCase;
+
+  PokedexBloc(this._basicPokemonsUseCase) : super(const PokedexState()) {
+    on<LoadPokedexPage>(_loadNewPage);
+    add(LoadPokedexPage());
+  }
+
+  void _loadNewPage(LoadPokedexPage event, Emitter<PokedexState> emit) async {
+    emit(state.copyWith(isLoading: true));
+    final nextPage = state.currentPage + 1;
+    final pokemons = await _basicPokemonsUseCase(page: nextPage);
+    //TODO: handle errors
+    emit(state.copyWith(
+      isLoading: false,
+      pokemons: pokemons,
+      currentPage: nextPage,
+    ));
   }
 }
